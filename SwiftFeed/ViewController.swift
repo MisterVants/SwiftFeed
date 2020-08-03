f6f0f05c1a7abc9a52b37c5dd9dda5d70b78a5e7
MrVants <andrevants@gmail.com>
2020-08-03 12:50:44 -0300

    Initial commit

---
+++ SwiftFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftFeed
+//
+//  Created by André Vants Soares de Almeida on 03/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
